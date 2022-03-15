0c9090fa1e12e1ffbfa1eea13ae035f52b1209eb
cuong <cuongnguyen>
2022-03-15 19:16:19 +0700

    update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ rule.minute = 19;
 rule.tz = 'Asia/Phnom_Penh';
 var j = schedule.scheduleJob(rule, function(fireDate){
     console.log('running job!');
-    // let payload = { text: 'Commit code và điền daily report trước khi checkout!'};
-    // let res = axios.post("https://chat.googleapis.com/v1/spaces/AAAAjBdDXBs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=IQ432w7v-UFEFZqHFuVdNvMIWhacK0FvQArgnusjZfs%3D", payload);
-    // let data = res.data;
+    let payload = { text: 'Commit code và điền daily report trước khi checkout!'};
+    let res = axios.post("https://chat.googleapis.com/v1/spaces/AAAAjBdDXBs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=IQ432w7v-UFEFZqHFuVdNvMIWhacK0FvQArgnusjZfs%3D", payload);
+    let data = res.data;
     console.log(fireDate)
 });
