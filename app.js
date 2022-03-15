ce7cf0744d20bb3baac75138052eb1d77b26689e
cuong <cuongnguyen>
2022-03-15 17:08:48 +0700

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const schedule = require('node-schedule');
+const axios = require('axios');
+const rule = new schedule.RecurrenceRule();
+rule.hour = 17;
+rule.minute = 5;
+rule.tz = 'Asia/Phnom_Penh';
+var j = schedule.scheduleJob(rule, function(fireDate){
+    console.log('running job!');
+    let payload = { text: 'Commit code và điền daily report trước khi checkout!'};
+    let res = axios.post("https://chat.googleapis.com/v1/spaces/AAAAjBdDXBs/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=IQ432w7v-UFEFZqHFuVdNvMIWhacK0FvQArgnusjZfs%3D", payload);
+    let data = res.data;
+    console.log(fireDate)
+});
